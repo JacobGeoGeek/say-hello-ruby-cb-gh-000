b0c35b1d35ed6936c3c2210bd708dbf6fa54d6b9
jacob chaar <jacobchaar@live.ca>
2017-12-29 17:40:32 +0000

    automatically backed up by learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper'
 
-describe "say_hello" do 
-  
+describe "say_hello" do
+
   it 'accepts an argument of a name and prints out Hello with that Name' do
     #result1 = say_hello("Kent Beck")
     #expect(result1).to eq("Hello Kent Beck!")
