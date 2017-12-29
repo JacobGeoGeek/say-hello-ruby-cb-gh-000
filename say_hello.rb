453721ff9523a6cff2687bb10d36fb558e59023c
jacob chaar <jacobchaar@live.ca>
2017-12-29 17:35:57 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,5 @@
 # Build your say_hello method here
-def say_hello(x="Ruby Programmer")
-  print("Hello  #{x}!")
+def say_hello(name="Ruby Programmer")
+  result = "Hello #{name}!"
+  return result
 end
